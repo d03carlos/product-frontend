c199f49a6e8facbae12d0a458ce39f70d0aaadc3
fidel laura <lsfidel2@gmail.com>
2023-09-23 19:11:21 -0500

    angular list-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaProductosComponent } from './lista-productos/lista-productos.component';
+import { RegistroProductosComponent } from './registro-productos/registro-productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaProductosComponent,
+    RegistroProductosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/registro-productos/registro-productos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-productos',
+  templateUrl: './registro-productos.component.html',
+  styleUrls: ['./registro-productos.component.css']
+})
+export class RegistroProductosComponent {
+
+}
